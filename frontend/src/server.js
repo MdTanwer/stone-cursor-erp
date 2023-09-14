802392a7d71d141d72261f2f47cc2689a829be62
Mohammad <saifuddin.md.er@gmail.com>
2023-09-14 10:58:37 +0530

    edits in .env

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -3,10 +3,10 @@
 export const server =
   process.env.REACT_APP_NODE_ENV === 'DEVELOPMENT'
     ? 'http://localhost:8000/api/v2'
-    : '';
+    : 'https://stone-crusher-erp-base-api.vercel.app/api/v2';
 // export const backend_url = "https://role-based-eshop-framework.vercel.app/";
 // export const backend_url = "http://localhost:8000/";
 export const backend_url =
   process.env.REACT_APP_NODE_ENV === 'DEVELOPMENT'
     ? 'http://localhost:8000/'
-    : '';
+    : 'https://stone-crusher-erp-base-api.vercel.app';
